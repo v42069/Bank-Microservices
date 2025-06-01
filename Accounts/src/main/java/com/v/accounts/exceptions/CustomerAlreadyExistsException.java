6c62d9abd876248f9ab2d4e9975b9db9d68ea196
Vighnesh Tathare <v42069@yahoo.com>
2025-06-01 21:27:14 +0530

    Cleaned repository

--- Accounts/src/main/java/com/v/accounts/exceptions/CustomerAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.v.accounts.exceptions;
-
-public class CustomerAlreadyExistsException extends RuntimeException{
-	
-	public CustomerAlreadyExistsException(String messege) {
-		super(messege);
-	}
-
-}
