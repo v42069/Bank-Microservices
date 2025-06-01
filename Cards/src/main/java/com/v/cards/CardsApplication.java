ed204874c01a772e97b62f8c82c6c5bcd86f0cab
Vighnesh Tathare <v42069@yahoo.com>
2025-06-01 22:05:44 +0530

    cards

---
+++ Cards/src/main/java/com/v/cards/CardsApplication.java
@@ -0,0 +1,17 @@
+package com.v.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
