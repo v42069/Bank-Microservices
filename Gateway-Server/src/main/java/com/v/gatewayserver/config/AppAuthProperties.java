08e2414050329152a3abf77d119fda8a26d60cda
Vighnesh Tathare <v42069@yahoo.com>
2025-06-30 21:28:28 +0530

    added security

---
+++ Gateway-Server/src/main/java/com/v/gatewayserver/config/AppAuthProperties.java
@@ -0,0 +1,23 @@
+package com.v.gatewayserver.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@ConfigurationProperties(prefix = "keycloak")
+public class AppAuthProperties {
+
+    private String clientId;
+    private String clientSecret;
+    private String tokenUrl;
+
+    // Getters and Setters
+    public String getClientId() { return clientId; }
+    public void setClientId(String clientId) { this.clientId = clientId; }
+
+    public String getClientSecret() { return clientSecret; }
+    public void setClientSecret(String clientSecret) { this.clientSecret = clientSecret; }
+
+    public String getTokenUrl() { return tokenUrl; }
+    public void setTokenUrl(String tokenUrl) { this.tokenUrl = tokenUrl; }
+}
