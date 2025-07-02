49237d19c7d10f97582eed59a2817f9e4b6fe0ba
Vighnesh Tathare <v42069@yahoo.com>
2025-07-02 23:57:42 +0530

    added security

--- Gateway-Server/src/main/java/com/v/gatewayserver/GatewayServerApplication.java
+++ Gateway-Server/src/main/java/com/v/gatewayserver/GatewayServerApplication.java
@@ -15,9 +15,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.v.gatewayserver.config.AppAuthProperties;
-import com.v.gatewayserver.filters.JwtAuthGatewayFilterFactory;
+
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
@@ -55,28 +56,29 @@ public class GatewayServerApplication {
 	}
 
 	@Bean
+
 	public RouteLocator vBanksConfig(RouteLocatorBuilder routeLocatorBuilder) {
 		return routeLocatorBuilder.routes()
 				.route(p -> p.path("/v/accounts/**")
 						.filters(f -> f.rewritePath("/v/accounts/(?<segment>.*)", "/${segment}")
-								.filter(new JwtAuthGatewayFilterFactory(appAuthProperties)
-										.apply(new JwtAuthGatewayFilterFactory.Config()))
+//								.filter(new JwtAuthGatewayFilterFactory(appAuthProperties)
+//										.apply(new JwtAuthGatewayFilterFactory.Config()))
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
 										.setFallbackUri("forward:/contactSupport")))
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p.path("/v/cards/**")
 						.filters(f -> f.rewritePath("/v/cards/(?<segment>.*)", "/${segment}")
-								.filter(new JwtAuthGatewayFilterFactory(appAuthProperties)
-										.apply(new JwtAuthGatewayFilterFactory.Config()))
+//								.filter(new JwtAuthGatewayFilterFactory(appAuthProperties)
+//										.apply(new JwtAuthGatewayFilterFactory.Config()))
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
 										.setKeyResolver(userKeyResolver())))
 						.uri("lb://CARDS"))
 				.route(p -> p.path("/v/loans/**")
 						.filters(f -> f.rewritePath("/v/loans/(?<segment>.*)", "/${segment}")
-								.filter(new JwtAuthGatewayFilterFactory(appAuthProperties)
-										.apply(new JwtAuthGatewayFilterFactory.Config()))
+//								.filter(new JwtAuthGatewayFilterFactory(appAuthProperties)
+//										.apply(new JwtAuthGatewayFilterFactory.Config()))
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
 										.setBackoff(Duration.ofMillis(100), Duration.ofSeconds(2), 2, true)))
