0ad99a6141c05be3532066e3fc633b64f760c722
Vighnesh Tathare <v42069@yahoo.com>
2025-07-08 23:38:39 +0530

    added messaging

---
+++ Accounts/src/main/java/com/v/accounts/dto/AccountsMsgDto.java
@@ -0,0 +1,10 @@
+package com.v.accounts.dto;
+
+/**
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
+}
